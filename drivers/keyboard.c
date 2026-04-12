394a712c79cca5a6818923ed823154b266f4804b
pxxelina <mac@MacBook-Air-de-MAC.local>
2026-04-12 15:28:12 +0100

    feat: add keyboard driver

---
+++ drivers/keyboard.c
@@ -0,0 +1,16 @@
+unsigned char inb(unsigned short port) {
+    unsigned char val;
+    __asm__ volatile ("inb %1, %0":"=a"(val):"Nd"(port));
+    return val;
+}
+
+char read_key() {
+    char keys[] = "??1234567890-=??qwertyuiop[]??asdfghjkl;~`?\\zxcvbnm,./? ?";
+    unsigned char sc;
+    do { while((inb(0x64)&0x01)==0); sc=inb(0x60); } while(sc&0x80);
+    if(sc==0x1C) return 10;
+    if(sc==0x0E) return 8;
+    if(sc==0x39) return 32;
+    if(sc<58) return keys[sc];
+    return 63;
+}
